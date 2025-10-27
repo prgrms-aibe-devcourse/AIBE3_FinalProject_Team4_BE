2db6b629251c17679ae2984563c24d18248c34a8
haemin4738 <leeheamin12@gmail.com>
2025-10-27 16:18:25 +0900

    [Init] 초기설정

---
+++ src/main/java/com/back/Aibe3FinalProjectTeam4Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aibe3FinalProjectTeam4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aibe3FinalProjectTeam4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/Aibe3FinalProjectTeam4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aibe3FinalProjectTeam4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
