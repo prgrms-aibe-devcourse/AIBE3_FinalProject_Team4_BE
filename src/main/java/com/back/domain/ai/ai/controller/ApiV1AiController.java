fc053232a4fa4501fb4a5d00353cc2ab23c1195a
leejiyeon <sky75013@naver.com>
2025-11-06 23:54:00 +0900

    feat: 모델 선택

--- src/main/java/com/back/domain/ai/ai/controller/ApiV1AiController.java
+++ src/main/java/com/back/domain/ai/ai/controller/ApiV1AiController.java
@@ -3,6 +3,8 @@ package com.back.domain.ai.ai.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class ApiV1AiController {
+    @Qualifier("openAiChatClient")
     private final ChatClient openAiChatClient;
+    @Qualifier("huggingfaceChatClient")
+    private final ChatClient huggingfaceChatClient;
 
     @GetMapping("/ai")
     String generation(String userInput) {
         return this.openAiChatClient.prompt()
+                .options(OpenAiChatOptions.builder().model("gpt-4o-mini").build())
                 .user(userInput)
                 .call()
                 .content();
