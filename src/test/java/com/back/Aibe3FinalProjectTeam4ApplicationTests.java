2ebbe5604611ecdeafd7410140a36b6b89dde96f
jooky <jooky0327@naver.com>
2025-11-12 10:26:53 +0900

    3차 PR check failed 테스트

--- src/test/java/com/back/Aibe3FinalProjectTeam4ApplicationTests.java
+++ src/test/java/com/back/Aibe3FinalProjectTeam4ApplicationTests.java
@@ -5,6 +5,7 @@ import com.back.domain.user.mail.service.VerificationTokenService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -12,7 +13,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 @SpringBootTest
 class Aibe3FinalProjectTeam4ApplicationTests {
 
-    static class Config {
+    @TestConfiguration
+    static class TestBeanConfig {
         @Bean
         public JavaMailSender javaMailSender() {
             return Mockito.mock(JavaMailSender.class);
