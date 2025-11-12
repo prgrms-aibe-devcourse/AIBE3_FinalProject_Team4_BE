010e7063c341182ae45b9eacdbe98a1cfe48dcec
yeonseo <lay3699@naver.com>
2025-11-12 16:39:47 +0900

    🔄refactor: data.sql 파일위치 변경

--- src/main/resources/data/data.sql
+++ src/main/resources/data/data.sql
@@ -3,7 +3,7 @@ VALUES ('user1', '1234', 'user1@test.com');
 
 INSERT INTO hashtags (name)
 VALUES ('#운동'),
-       ('아키텍쳐');
+       ('#아키텍쳐');
 
 INSERT INTO blogs (user_id, title, content, status, view_count, created_at)
 VALUES (1, '첫 번째 블로그 글', '새로운 운동을 시작했습니다.', 'PUBLISHED', 0, NOW());
