9ec3491bfefab8c5192ac9c1ebe844f08b33326d
jookwonyoung <jooky0327@naver.com>
2025-11-12 10:46:41 +0900

    [Feat] 회원 이메일 인증 관련 기능 (#22)
    * feat: 이메일 인증코드 전송, 인증코드 검증 API 구현
    
    * fix: 이메일 중복 가능하게 수정
    
    * 이메일 인증 결과로 얻은 토큰값을 dto에 넣어야 회원가입 가능
    
    * feat: 비밀번호 초기화 API 구현(이메일 인증 해야 가능)
    
    * 커밋 revert 및 피드백 반영
    
    * 5차 PR check failed 테스트
    
    * ci.yml 더미값으로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
     compileOnly("org.projectlombok:lombok")
@@ -45,6 +46,9 @@ dependencies {
 
     // Swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+
+    // Mail
+    implementation("org.springframework.boot:spring-boot-starter-mail")
 
     // JWT
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
