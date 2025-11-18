5763c5fb471bbe4fbc44efb46fbb90dc6f1b6e7e
jooky <jooky0327@naver.com>
2025-11-18 10:21:42 +0900

    엔티티 생성

---
+++ src/main/java/com/back/domain/user/follow/entity/Follow.java
@@ -0,0 +1,40 @@
+package com.back.domain.user.follow.entity;
+
+import com.back.domain.user.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "follow", uniqueConstraints = {@UniqueConstraint(columnNames = {"follower_id", "following_id"})})
+public class Follow{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id")
+    private User follower;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_id")
+    private User following;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+
+    public static Follow createFollow(User follower, User following) {
+        return new Follow(null, follower, following, null);
+    }
+
+}
