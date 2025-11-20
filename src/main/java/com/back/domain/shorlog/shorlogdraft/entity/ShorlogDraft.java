312fac85ad7fe98db01f192481051675bf4ef793
haemin4738 <leeheamin12@gmail.com>
2025-11-20 11:48:56 +0900

    [Refactor] ut 폴더 병합

--- src/main/java/com/back/domain/shorlog/shorlogdraft/entity/ShorlogDraft.java
+++ src/main/java/com/back/domain/shorlog/shorlogdraft/entity/ShorlogDraft.java
@@ -2,7 +2,7 @@ package com.back.domain.shorlog.shorlogdraft.entity;
 
 import com.back.domain.user.user.entity.User;
 import com.back.global.jpa.entity.BaseEntity;
-import com.back.global.util.JsonUtil;
+import com.back.global.ut.JsonUtil;
 import jakarta.persistence.*;
 import lombok.*;
 
