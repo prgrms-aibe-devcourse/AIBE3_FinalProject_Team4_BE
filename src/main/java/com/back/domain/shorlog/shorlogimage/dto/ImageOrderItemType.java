73a1923c055c1140af3043020260831077206ecb
get783 <81467332+get783@users.noreply.github.com>
2025-11-25 11:42:44 +0900

    [Feat] 이미지URL 파일 변환 (#76)
    * feat: 무료 이미지 api로 가져온 url로 블로그 이미지 업로드하기
    
    * fix: 이미지 url 다운로드 시 기본 버퍼 크기 제한 늘림
    
    * refactor: 파일 image 패키지로 이동
    
    * feat: 블로그 이미지 업로드 시 URL인 경우 파일 변환 후 저장
    
    * chore: 몇번째 이미지에서 성공/실패 여부 알기 위한 업로드 시작 로그 출력
    
    * chore: 필요없는 파일 삭제
    
    * chore: MockMultipartFile 사용 의존성 추가
    
    * refactor: MockMultipartFile 삭제
    
    * refactor: 요청 시 type 대소문자 둘다 가능
    
    * refactor: 이름 변경
    
    * refactor: 파일 리스트에서 꺼내올 때 조건문 추가
    
    * refactor: 조건문 수정
    
    * refactor: 클래스명 변경 및 패키지 이동
    
    * refactor: 변수명 수정

---
+++ src/main/java/com/back/domain/shorlog/shorlogimage/dto/ImageOrderItemType.java
@@ -0,0 +1,31 @@
+package com.back.domain.shorlog.shorlogimage.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum ImageOrderItemType {
+    FILE("file"),
+    URL("url");
+
+    private final String value;
+
+    ImageOrderItemType(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static ImageOrderItemType from(String input) {
+        if (input == null) return null;
+
+        for (ImageOrderItemType type : values()) {
+            if (type.value.equalsIgnoreCase(input)) return type;
+        }
+
+        return ImageOrderItemType.valueOf(input.toUpperCase());
+    }
+}
