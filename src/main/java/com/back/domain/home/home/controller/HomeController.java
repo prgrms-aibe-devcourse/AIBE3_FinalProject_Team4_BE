ea8b4511828808543ed7d972e6021b76b5a85332
haemin4738 <leeheamin12@gmail.com>
2025-11-26 15:28:50 +0900

    [Feat] 배포 application prod 수정

--- src/main/java/com/back/domain/home/home/controller/HomeController.java
+++ src/main/java/com/back/domain/home/home/controller/HomeController.java
@@ -1,21 +1,35 @@
 package com.back.domain.home.home.controller;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
+import java.net.InetAddress;
+
+import static java.net.InetAddress.getLocalHost;
+import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
 
 @RestController
 @Slf4j
+@Tag(name="HomeController", description = "홈 컨트롤러")
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        LocalDateTime localDateTime = LocalDateTime.now();
-        log.info("Home " + localDateTime);
-        return "Hello, World!";
-    }
+    @SneakyThrows
+    @GetMapping(produces = TEXT_HTML_VALUE)
+    @Operation(summary = "메인 페이지")
+    public String main() {
+        InetAddress localHost = getLocalHost();
 
+        return """
+                <h1>API 서버</h1>
+                <p>Host Name: %s</p>
+                <p>Host Address: %s</p>
+                <div>
+                    <a href="/swagger-ui/index.html">API 문서로 이동</a>
+                </div>
+                """.formatted(localHost.getHostName(), localHost.getHostAddress());
+    }
 }
