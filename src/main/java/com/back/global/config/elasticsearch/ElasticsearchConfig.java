83e27f59ec874decfd4e5687adbb2b2bbfaf1931
haemin4738 <leeheamin12@gmail.com>
2025-12-01 02:05:11 +0900

    [Fix] ElastcisearchConfig 비활성화

--- src/main/java/com/back/global/config/elasticsearch/ElasticsearchConfig.java
+++ src/main/java/com/back/global/config/elasticsearch/ElasticsearchConfig.java
@@ -1,14 +1,14 @@
-package com.back.global.config.elasticsearch;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-@Configuration
-@EnableElasticsearchRepositories(basePackages = {
-        "com.back.domain.blog.blogdoc.repository",
-        "com.back.domain.shorlog.shorlogdoc.repository"
-})
-//@ConditionalOnProperty(name = "elasticsearch.enabled", havingValue = "true")
-public class ElasticsearchConfig {
-}
-
+//package com.back.global.config.elasticsearch;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+//
+//@Configuration
+//@EnableElasticsearchRepositories(basePackages = {
+//        "com.back.domain.blog.blogdoc.repository",
+//        "com.back.domain.shorlog.shorlogdoc.repository"
+//})
+////@ConditionalOnProperty(name = "elasticsearch.enabled", havingValue = "true")
+//public class ElasticsearchConfig {
+//}
+//
