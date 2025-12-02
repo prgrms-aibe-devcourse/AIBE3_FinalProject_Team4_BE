42b93353feeb6ad7e3857cd9c27083583c267b8e
haemin4738 <leeheamin12@gmail.com>
2025-12-02 15:43:06 +0900

    [Feat] 대댓글 단계, 자신이 작성한 댓글인지 확인

--- src/main/java/com/back/domain/comments/comments/exception/CommentsErrorCase.java
+++ src/main/java/com/back/domain/comments/comments/exception/CommentsErrorCase.java
@@ -18,6 +18,7 @@ public enum CommentsErrorCase implements ErrorCase {
     COMMENT_LIKE_FORBIDDEN(HttpStatus.FORBIDDEN, 7006, "자신의 댓글에는 좋아요를 누를 수 없습니다."),
     COMMENT_LIKE_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, 7007, "이미 좋아요를 누른 댓글입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, 7008, "해당 사용자를 찾을 수 없습니다."),
+    REPLY_DEPTH_EXCEEDED(HttpStatus.BAD_REQUEST, 7008, "대댓글은 한 단계까지만 작성할 수 있습니다."),
     TARGET_NOT_FOUND(HttpStatus.NOT_FOUND, 7008, "대상 게시글을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
